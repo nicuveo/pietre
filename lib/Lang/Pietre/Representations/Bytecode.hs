82ff44ba60b082e7c70888540dfbb347e055c055
Antoine Leblanc <crucuny@gmail.com>
2025-04-22 18:43:46 +0100

    add bytecode

--- lib/Lang/Pietre/Representations/Bytecode.hs
+++ lib/Lang/Pietre/Representations/Bytecode.hs
@@ -2,7 +2,47 @@ module Lang.Pietre.Representations.Bytecode where
 
 import "this" Prelude
 
-data Instruction addr
-  = Push Int
+import Data.Kind      (Type)
+
+
+data LinkerPhase = Unresolved | Resolved
+
+class
+  ( Show (PushAddress     p)
+  , Show (EntranceAddress p)
+  ) => PhaseTypes (p :: LinkerPhase) where
+  type PushAddress     p :: Type
+  type EntranceAddress p :: Type
+
+instance PhaseTypes Unresolved where
+  type PushAddress     Unresolved = Text
+  type EntranceAddress Unresolved = Text
+
+instance PhaseTypes Resolved where
+  type PushAddress     Resolved = Void
+  type EntranceAddress Resolved = ()
+
+
+data Instruction (phase :: LinkerPhase)
+  = PushInt  Int
+  | PushAddr ~(PushAddress phase)
   | Pop
-  | Call addr
+  | Add
+  | Subtract
+  | Multiply
+  | Divide
+  | Mod
+  | Not
+  | Greater
+  | Duplicate
+  | Roll
+  | InInt
+  | InChar
+  | OutInt
+  | OutChar
+  | Entrance ~(EntranceAddress phase)
+  | Branch
+  | Return
+  | Terminate
+
+deriving instance PhaseTypes p => Show (Instruction p)
