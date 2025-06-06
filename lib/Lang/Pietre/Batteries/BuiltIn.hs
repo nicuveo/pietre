febff1c247246da3d07dd387c95ac7e4db05bf59
Antoine Leblanc <crucuny@gmail.com>
2025-06-06 01:51:35 +0100

    add notion of reserved identifiers

--- lib/Lang/Pietre/Batteries/BuiltIn.hs
+++ lib/Lang/Pietre/Batteries/BuiltIn.hs
@@ -4,8 +4,12 @@ module Lang.Pietre.Batteries.BuiltIn where
 
 import "this" Prelude
 
+import Data.HashSet                       qualified as S
+import Data.List.NonEmpty                 qualified as NE
+
 import Lang.Pietre.Representations.AST
 import Lang.Pietre.Representations.Name
+import Lang.Pietre.Representations.Tokens
 
 
 builtins :: [(Path, Name)]
@@ -14,6 +18,12 @@ builtins =
   , (pure "char", _pathName CharType)
   , (pure "bool", _pathName BoolType)
   ]
+
+reserved :: HashSet Identifier
+reserved = S.fromList $ map (NE.head . fst) builtins
+
+isReserved :: Identifier -> Bool
+isReserved = flip S.member reserved
 
 pattern IntType  :: PathInfo Resolved
 pattern IntType  = PathInfo (BuiltinType "int")  []
